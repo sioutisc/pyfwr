8057e3fca9b298e353802e03933e38c19887a428
Christos Sioutis <christos.sioutis@gmail.com>
2017-06-10 22:14:07 +0930

    added Search

---
+++ example-Search.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# =======================================================================
+# Copyright 2017 Christos Sioutis <christos.sioutis@gmail.com>
+# =======================================================================
+# This file is part of pyfwr.
+#
+# pyfwr is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# pyfwr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with pyfwr.
+# If not, see <http://www.gnu.org/licenses/>.
+# =======================================================================
+
+import pyfwr
+
+c = {'username':'your_username','apikey':'your_apikey'}
+
+req = { 'credentials': c , 'operation': 'Search' , 'inputs': {'query':'-latlong "-36 137 -33 139"'} }
+response = pyfwr.get(req)
+print '================= JSON RESPONSE ==================='
+print response
+print '==================================================='
+for flight in response['SearchResult']['aircraft']:
+	print 'Flight ' + flight['ident'] \
+		+ ' of type ' + flight['type'] \
+		+ ' at location ' + str(flight['latitude']) + ',' + str(flight['longitude']) \
+		+ ' with altitude ' + str(flight['altitude'])
